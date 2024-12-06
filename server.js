8087e980380a2fc7f4eb5d95cd264179276abf66
irene-nakabugo <irenenakabugo0@gmail.com>
2024-12-06 10:58:55 +0300

    installed mongoose

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 require('dotenv/config')
-
+const mongoose = require('mongoose')
 
 // creating an express app
 const app = express()
@@ -13,6 +13,12 @@ const app = express()
 app.get('/', (req,res)=>{
     res.send('Hey Code Queen');
 })
+
+// CONNECT TO THE DATABASE
+mongoose.connect(process.env.MONGODB_LINK)
+.then(()=>{console.log('Connected to the database')})
+.catch(err=>{console.log('ERROR',err)})
+
 
 app.listen(process.env.PORT, ()=>{console.log('connected to the port')});
 
